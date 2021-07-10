ab8f1cc8fd61c80d67211dc8d225f9a39987a7ec
Shashank <shashankthakur462@gmail.com>
2021-07-10 15:48:27 +0530

    Update DoormatProblem.py

--- DoormatProblem.py
+++ DoormatProblem.py
@@ -1,6 +1,6 @@
-N,M = map(int,input().split(" "))
-for i in range(1,N,2):
-    print((i * ".|.").center(M, "-"))
-print("WELCOME".center(M,"-"))
-for i in range(N-2,-1,-2):
+N,M = map(int,input().split(" "))    #Taking the input
+for i in range(1,N,2):   #Loop for upper design
+    print((i * ".|.").center(M, "-"))  
+print("WELCOME".center(M,"-"))  #Printing the middle design
+for i in range(N-2,-1,-2):    #Loop for lower design
     print((i * ".|.").center(M, "-"))    
